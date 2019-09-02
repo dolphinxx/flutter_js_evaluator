04819bf08d4db296547f762941d8bfb60250d823
dolphin <adaikiss@163.com>
2019-09-02 09:39:02 +0800

    switch swift to objective-c

--- ios/Classes/FlutterJsEvaluatorPlugin.m
+++ ios/Classes/FlutterJsEvaluatorPlugin.m
@@ -1,8 +1,20 @@
 #import "FlutterJsEvaluatorPlugin.h"
-#import <flutter_js_evaluator/flutter_js_evaluator-Swift.h>
 
 @implementation FlutterJsEvaluatorPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterJsEvaluatorPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_js_evaluator"
+            binaryMessenger:[registrar messenger]];
+  FlutterJsEvaluatorPlugin* instance = [[FlutterJsEvaluatorPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
